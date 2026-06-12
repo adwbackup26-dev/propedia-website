75a8c1fa86e5d9426c6bf4ce20b6380105e79ce5
Anirudha Warhadpande <anirudha@nimet.local>
2026-06-12 19:20:39 -0400

    Remove unsupported LivingArea field

--- api/listings.js
+++ api/listings.js
@@ -102,7 +102,6 @@ export default async function handler(req, res) {
     'BathroomsTotalInteger',
     'PropertyType',
     'PropertySubType',
-    'LivingArea',
     'DaysOnMarket',
     'InternetEntireListingDisplayYN',
     'ListAgentFullName',
